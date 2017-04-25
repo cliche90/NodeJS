93462740a8ccc657213332bc837e8992efd4ba76
snacky <cliche90@naver.com>
2017-04-25 19:09:02 +0900

    Modify app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));  // Directory 명을 넣어두면 정적인 파일 서비스 가능
 app.get('/', function(req, res){
     res.send('Hello home page');
+});
+app.get('/github_logo', function(req, res){
+    res.send('Hello Github!  <img src="/github_logo.png">');
 });
 app.get('/login', function(req, res){
     res.send('<h1>Login please<h1>');
@@ -10,4 +14,3 @@ app.get('/login', function(req, res){
 app.listen(3000, function(){
     console.log('Connected 3000 port!');
 });
-
