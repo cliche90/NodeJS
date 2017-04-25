a6c6029e4c7214807641d4c00b84cb9995b0f551
snacky <cliche90@naver.com>
2017-04-25 19:52:35 +0900

    Modify app.js

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
 var express = require('express');
 var app = express();
-
+app.set('view engine', 'jade');
+app.set('views', './views');        // 없어도 자동으로 views 디렉토리를 찾는다.
 app.use(express.static('public'));  // Directory 명을 넣어두면 정적인 파일 서비스 가능
+app.get('/template', function(req, res){
+    res.render('temp');
+});
+
 app.get('/', function(req, res){
     res.send('Hello home page');
 });
+app.get('/dynamic', function(req, res){
+    var lis = '';
+    for(var i=0; i<5; i++){
+        lis = lis + '<li>coding</li>';
+    }
+    var time = Date();
+    var output = `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title></title>
+        </head>
+        <body>
+            Hello, Dynamic!
+            <ul>
+                ${lis}
+            </ul>
+            ${time}
+        </body>
+    </html>
+    `;
+    res.send(output);
+});
 app.get('/github_logo', function(req, res){
-    res.send('Hello Github!  <img src="/github_logo.png">');
+     res.send('Hello Github!  <img src="/github_logo.png">');
 });
 app.get('/login', function(req, res){
     res.send('<h1>Login please<h1>');
